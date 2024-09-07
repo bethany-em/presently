6e64fe6345a5fcbface58b5811209723c2646ef6
Brian Park <park-brian@users.noreply.github.com>
2024-09-07 01:44:37 -0400

    pass view state via function call from parent

--- components/view.js
+++ components/view.js
@@ -5,67 +5,47 @@ import htm from "htm";
 const html = htm.bind(h);
 
 export default function View() {
-  const [stream, setStream] = useState(null);
-  const [state, setState] = useState({
+  const [state, setState] = useState(window.viewState || {
+    slide: null,
     slideObject: null,
     presentationObject: null,
     showFullVideo: false,
-    videoDeviceId: null,
+    videoSource: null,
   });
-  const { slide, slideObject, presentationObject, showFullVideo, videoDeviceId } = state;
-  const presentationIndex = slide?.[1];
+  const { slide, slideObject, presentationObject, showFullVideo, videoSource } = state;
   const slideIndex = slide?.[3];
 
-
   useEffect(() => {
-    if (videoDeviceId) {
-      navigator.mediaDevices
-        .getUserMedia({
-          video: {
-            deviceId: videoDeviceId,
-          },
-        })
-        .then((stream) => {
-          setStream(stream);
-        });
-    } else {
-      setStream(null);
-    }
-  }, [videoDeviceId]);
+    // initialize view state on load
+    window.viewState && setState(window.viewState);
 
-  useEffect(() => {
-    const onMessage = (event) => {
-      console.log("message", event.data);
-      setState(event.data);
-    };
-    window.addEventListener("message", onMessage);
-    return () => window.removeEventListener("message", onMessage);
+    // register view state listener
+    window.setViewState = setState;
+    return () => window.setViewState = null;
   }, [setState]);
-
-  // console.log({ slideObject });
 
   return html`
     ${slideObject &&
     html`<div
       class=${[
         "text-white position-absolute bottom-0 left-0 d-flex justify-content-center w-100 z-1 text-pre-wrap text-center fw-semibold h-100 text-shadow bg-gradient-dark",
-        stream ? "align-items-end" : "align-items-center",
+        videoSource?.stream ? "align-items-end" : "align-items-center",
       ].join(" ")}
       style=${{
-        fontSize: "6vh",
-        padding: "5vh"
+        fontSize: "6.5vh",
+        padding: "5vh",
       }}>
       ${slideObject.content}
     </div>`}
     ${presentationObject?.attribution &&
     slideIndex === 0 &&
-    html` <div
+    html`<div
       class="position-absolute bottom-0 left-0 w-100 z-1 text-white text-pre-wrap opacity-25"
       style=${{ fontSize: "1.5vh", padding: "1vh" }}>
       ${presentationObject.attribution}
     </div>`}
-    ${stream &&
-    html` <video
+    ${videoSource?.stream &&
+    html`<video
       class=${[
         "h-100 w-100 position-absolute object-position-top",
         showFullVideo ? "object-fit-contain" : "object-fit-cover",
@@ -73,6 +53,7 @@ export default function View() {
       autoplay
       playsinline
       muted
-      srcObject=${stream} />`}
+      disablePictureInPicture       
+      srcObject=${videoSource?.stream} />`}
   `;
 }
