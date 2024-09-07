30c823080afc81d272d09d9e942f88cbc81e34d6
Brian Park <park-brian@users.noreply.github.com>
2024-09-07 02:35:47 -0400

    add video toggles for stage/audience view

--- components/preview.js
+++ components/preview.js
@@ -13,19 +13,25 @@ export default function Preview({ deck, slide, videoSource, height = 270, width 
   const [stageWindow, setStageWindow] = useState(null);
   const [audienceWindow, setAudienceWindow] = useState(null);
   const [showFullVideo, setShowFullVideo] = useState(false);
+  const [showAudienceVideo, setShowAudienceVideo] = useState(true);
+  const [showStageVideo, setShowStageVideo] = useState(false);
 
   const presentationIndex = slide?.[1];
   const slideIndex = slide?.[3];
 
   useEffect(() => {
     const viewState = { deck, slide, presentationObject, slideObject, showFullVideo, videoSource };
+    const stageViewState = showStageVideo ? viewState : { ...viewState, videoSource: null };
+    const audienceViewState = showAudienceVideo ? viewState : { ...viewState, videoSource: null };
+
     // preload view state
-    if (stageWindow) stageWindow.viewState = viewState;
-    if (audienceWindow) audienceWindow.viewState = viewState;
-    stageWindow?.setViewState?.(viewState);
-    audienceWindow?.setViewState?.(viewState);
-    stagePreviewRef.current?.contentWindow?.setViewState?.(viewState);
-    audiencePreviewRef?.current?.contentWindow?.setViewState?.(viewState);
+    if (stageWindow) stageWindow.viewState = stageViewState;
+    stageWindow?.setViewState?.(stageViewState);
+    stagePreviewRef.current?.contentWindow?.setViewState?.(stageViewState);
+
+    if (audienceWindow) audienceWindow.viewState = audienceViewState;
+    audienceWindow?.setViewState?.(audienceViewState);
+    audiencePreviewRef?.current?.contentWindow?.setViewState?.(audienceViewState);
   }, [
     audienceWindow,
     stageWindow,
@@ -37,6 +43,8 @@ export default function Preview({ deck, slide, videoSource, height = 270, width 
     presentationObject,
     showFullVideo,
     videoSource,
+    showStageVideo,
+    showAudienceVideo,
   ]);
 
   return html`
@@ -55,24 +63,52 @@ export default function Preview({ deck, slide, videoSource, height = 270, width 
     </div>
 
     <div class="px-2 pt-4">
-      <div class="d-flex align-items-center mb-1">
-        <h1 class="h5 mb-0 me-2">Stage</h1>
-        <button
-          class="btn btn-sm btn-outline-warning fw-bold"
-          onClick=${() => setStageWindow(open("view.html?title=Stage", "StageView", `width=${width},height=${height}`))}>
-          Open
-        </button>
+      <div class="d-flex align-items-center justify-content-between mb-1" style=${{ width }}>
+        <div class="d-flex">
+          <h1 class="h5 mb-0 me-2">Stage</h1>
+          <button
+            class="btn btn-sm btn-outline-warning fw-bold"
+            onClick=${() =>
+              setStageWindow(open("view.html?title=Stage", "StageView", `width=${width},height=${height}`))}>
+            Open/Sync
+          </button>
+        </div>
+
+        <div class="form-check form-check-inline form-switch m-0">
+          <label class="fw-semibold small cursor-pointer" for="showStageVideo">Video</label>
+          <input
+            class="form-check-input cursor-pointer"
+            type="checkbox"
+            role="switch"
+            id="showStageVideo"
+            checked=${showStageVideo}
+            onChange=${() => setShowStageVideo(!showStageVideo)} />
+        </div>
       </div>
       <iframe ref=${stagePreviewRef} src="view.html" height=${height} width=${width} />
 
       <hr />
-      <div class="d-flex align-items-center mb-1">
-        <h1 class="h5 mb-0 me-2">Audience</h1>
-        <button
-          class="btn btn-sm btn-outline-warning fw-bold"
-          onClick=${() => setAudienceWindow(open("view.html?title=Audience", "AudienceView", `width=${width},height=${height}`))}>
-          Open
-        </button>
+      <div class="d-flex align-items-center justify-content-between mb-1" style=${{ width }}>
+        <div class="d-flex">
+          <h1 class="h5 mb-0 me-2">Audience</h1>
+          <button
+            class="btn btn-sm btn-outline-warning fw-bold"
+            onClick=${() =>
+              setAudienceWindow(open("view.html?title=Audience", "AudienceView", `width=${width},height=${height}`))}>
+            Open/Sync
+          </button>
+        </div>
+
+        <div class="form-check form-check-inline form-switch m-0">
+          <label class="fw-semibold small cursor-pointer" for="showAudienceVideo">Video</label>
+          <input
+            class="form-check-input cursor-pointer"
+            type="checkbox"
+            role="switch"
+            id="showAudienceVideo"
+            checked=${showAudienceVideo}
+            onChange=${() => setShowAudienceVideo(!showAudienceVideo)} />
+        </div>
       </div>
       <iframe ref=${audiencePreviewRef} src="view.html" height=${height} width=${width} />
     </div>
