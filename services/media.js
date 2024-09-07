57f1dc18dcdbcd0f2c146e100e1308a5c8ded713
Brian Park <park-brian@users.noreply.github.com>
2024-09-07 01:44:08 -0400

    update media service

--- services/media.js
+++ services/media.js
@@ -1,9 +1,15 @@
-export async function getVideoDevices() {
+export async function getUserMedia() {
   await navigator.mediaDevices.getUserMedia({ video: true }); // request access to video
   const devices = await navigator.mediaDevices.enumerateDevices();
   const videoDevices = devices.filter((device) => device.kind === "videoinput");
   for (const device of videoDevices) {
+    // assign stream to each device
     device.stream = await navigator.mediaDevices.getUserMedia({ video: device });
   }
   return videoDevices;
+}
+
+export async function getDisplayMedia() {
+  const stream = await navigator.mediaDevices.getDisplayMedia({ video: true });
+  return { stream, label: "Window" };
 }
