7908ab8e7da00295d7baeb5551ddd40008390137
Brian Park <park-brian@users.noreply.github.com>
2025-01-25 11:04:18 -0500

    capture specific resolutions

--- services/media.js
+++ services/media.js
@@ -2,12 +2,30 @@ export async function getUserMedia() {
   await navigator.mediaDevices.getUserMedia({ video: true }); // request access to video
   const devices = await navigator.mediaDevices.enumerateDevices();
   const videoDevices = devices.filter((device) => device.kind === "videoinput");
+  const resolutions = [
+    { width: {exact: 3840}, height: {exact: 2160 } },
+    { width: {exact: 1920}, height: {exact: 1080 } },
+    { width: {exact: 1600}, height: {exact: 1200 } },
+    { width: {exact: 1280}, height: {exact: 720 } },
+    { width: {exact: 800}, height: {exact: 600 } },
+    { width: {exact: 640}, height: {exact: 480 } },
+    {}
+  ];
   for (const device of videoDevices) {
-    try {
-      // assign stream to each device
-      device.stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: device.deviceId } });
-    } catch(e) {
-      console.log("error getting stream", e);
+    // assign highest-resolution stream to each device
+    for (const resolution of resolutions) {
+      try {
+        const constraints = {
+          video: {
+            deviceId: device.deviceId,
+            ...resolution
+          }
+        };
+        device.stream = await navigator.mediaDevices.getUserMedia(constraints);
+        break;
+      } catch (e) {
+        console.log("error getting stream", e);
+      }
     }
   }
   return videoDevices;
